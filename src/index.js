1816beeb2011b420965798bb734db984a22f9b92
Noah Scala <nwscala@gmail.com>
2020-03-27 14:47:50 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  const textbox = document.getElementById('new-task-description');
+  const tasksList = document.getElementById('tasks');
+  
+  
+  
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const newTask = document.createElement('li');
+    newTask.innerHTML = textbox.value;
+    tasksList.appendChild(newTask);
+  });
+
 });
